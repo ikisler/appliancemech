5bc2921a2d817eca9083a753218c129d195d9333
Isabeau Kisler <ikisler@csumb.edu>
2015-09-02 22:26:06 -0700

    Clean up git stuff

--- css/maktab/inc/lib/com/search-top.php
+++
@@ -1,6 +0,0 @@
-<div class="search-top-warp">
-  <form  method="get"   action="<?php echo home_url(); ?>/">
-    <input type="text" class="search-txt"  value="<?php echo __('enter search keyword','otouch');?>" onblur="if (this.value == ''){this.value = '<?php echo __('enter search keyword','otouch');?>'; }" onfocus="if (this.value == '<?php echo __('enter search keyword','otouch');?>') {this.value = ''; }"   name="s" id="s"/>
-    <input type="submit" id="header_search_submit" value="search"/>
-  </form>
-</div>
--- css/maktab/taxonomy-cat_portfolio.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-<div class="row">
-    <div class="span12">
-        <?php echo do_shortcode('[ott_portfolio]'); ?>
-    </div>
-</div>
-
-<?php get_footer(); ?>
